079b2513be850f07ebbf0d1b50124e517db44f25
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2021-08-27 16:15:59 -0500

    completed sumAll, leapYears, tempConversion & cal

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,22 @@
-const ftoc = function() {
+// const ftoc = function (f) {
+//   celcius = (f - 32) * (5 / 9);
+//   return celcius.toFixed(1);
+// };
 
+// const ctof = function (c) {
+//   farenheit = c * (9 / 5) + 32;
+//   return farenheit.toFixed(1);
+// };
+
+const ftoc = function (f) {
+  return Math.round((f - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  return Math.round((c * (9 / 5) + 32) * 10) / 10;
 };
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
